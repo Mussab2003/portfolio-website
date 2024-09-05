4b0eeee25d946ee39aa96f6079d5a2f4363e0104
Mussab2003 <muhammadmussab12345@gmail.com>
2024-09-05 16:13:48 +0500

    Updated Projects

---
+++ index.js
@@ -0,0 +1,8 @@
+//alert("Hello");
+$(".main-image").hide().fadeIn(5000);
+
+// $(window).on("scroll", function (){
+//     $(".main-image").animate({
+//         top: '+=1000px'
+//     }, 2000)
+// });
